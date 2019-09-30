e5122c5c8848ba29b5100157a4ca493792132855
SoraKumo <info@croud.jp>
2019-09-30 09:23:23 +0900

    モジュール連携に対応 クラスコンポーネントに対応

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,42 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useMemo, Component } from "react";
+import { useDispatch, useSelector, connect } from "react-redux";
 import { Dispatch } from "redux";
-import { useMemo } from "react";
+
+//対象オブジェクト選択用
+type Head<U> = U extends [any, ...any[]]
+  ? ((...args: U) => any) extends (head: infer H, ...args: any) => any
+    ? H
+    : never
+  : never;
+type Tail<U> = U extends [any, any, ...any[]]
+  ? ((...args: U) => any) extends (head: any, ...args: infer T) => any
+    ? T
+    : never
+  : never;
+type Deeps<State, Paths extends any[]> = Head<Paths> extends keyof State
+  ? {
+      0: State[Head<Paths>];
+      1: Deeps<State[Head<Paths>], Tail<Paths>>;
+    }[Tail<Paths> extends never ? 0 : 1]
+  : never;
 
 //パラメータをオプション化
 type AddOptionType<T> = {
   [M in keyof T]+?: T[M];
 };
 type map = { [key: string]: unknown };
+type moduleType<T> = {
+  new (
+    dispatch: Dispatch,
+    store: unknown,
+    prefix: string,
+    modules:
+      | undefined
+      | {
+          [key: string]: ReduxModule;
+        }
+  ): T;
+};
 
 /**
  *Callback用アクション定義
@@ -23,7 +53,7 @@ interface Action {
 const ActionName = "@CALLBACK";
 
 /**
- *Storeデータ設定用
+ *Storeデータ設定を行う
  *
  * @export
  * @template T
@@ -48,9 +78,14 @@ export function setStoreState<T = map>(
           const obj = {};
           tempState[name[i]] = obj;
           tempState = obj;
-        } else tempState = tempState[n] as map;
+        } else {
+          tempState[n] = { ...tempState[n] };
+          tempState = tempState[n] as map;
+        }
       }
-      tempState[name[i]] = { ...tempState[name[i]], ...params };
+      if (typeof params === "object")
+        tempState[name[i]] = { ...tempState[name[i]], ...params };
+      else tempState[name[i]] = params;
       return newState;
     } else {
       const newState = { ...state };
@@ -90,12 +125,22 @@ export function ModuleReducer(state = {}, action: Action) {
  */
 export class ReduxModule<State = { [key: string]: unknown }> {
   protected static defaultState?: unknown;
-  private dispatch: Dispatch<any>;
+  public static includes?: (
+    | typeof ReduxModule
+    | { module: typeof ReduxModule; prefix: string })[];
+  private dispatch: Dispatch;
   private store: unknown;
   private moduleName: string;
-  public constructor(dispatch: Dispatch<any>, store: unknown, moduleName: string) {
+  private modules?: { [key: string]: ReduxModule };
+  public constructor(
+    dispatch: Dispatch,
+    store: unknown,
+    moduleName: string,
+    modules: undefined | { [key: string]: ReduxModule }
+  ) {
     this.dispatch = dispatch;
     this.moduleName = moduleName;
+    this.modules = modules;
     if (store !== undefined) {
       this.store = store;
     } else {
@@ -109,6 +154,44 @@ export class ReduxModule<State = { [key: string]: unknown }> {
       }
     }
   }
+
+  /**
+   *includesに記述した外部参照モジュールを取得する
+   *
+   * @template T
+   * @template C
+   * @param {({
+   *       new (
+   *         dispatch: Dispatch,
+   *         store: unknown,
+   *         prefix: string,
+   *         modules?: { [key: string]: ReduxModule }
+   *       ): T;
+   *     } & C)} module
+   * @param {string} [prefix]
+   * @returns
+   * @memberof ReduxModule
+   */
+  getModule<T extends ReduxModule, C extends typeof ReduxModule>(
+    module: {
+      new (
+        dispatch: Dispatch,
+        store: unknown,
+        prefix: string,
+        modules?: { [key: string]: ReduxModule }
+      ): T;
+    } & C,
+    prefix?: string
+  ) {
+    const moduleName: string = getStoreName(module, prefix);
+    return this.modules![moduleName] as T;
+  }
+  /**
+   *ReduxのDispachを取得
+   *
+   * @returns
+   * @memberof ReduxModule
+   */
   public getDispatch() {
     return this.dispatch;
   }
@@ -120,12 +203,16 @@ export class ReduxModule<State = { [key: string]: unknown }> {
    * @returns {(T | undefined)}
    * @memberof StoreModule
    */
-  public getState<T = State>(): T | undefined;
-  public getState<T = unknown>(name?: string | string[]): T | undefined {
+  public getState<K extends string[], T = State>(): T | undefined;
+  public getState<K extends string[], T = State>(
+    ...name: K
+  ): Deeps<T, K> | undefined;
+  public getState<K extends string[], T = State>(
+    ...name: K
+  ): Deeps<T, K> | T | undefined {
     //パラメータ処理
-    let names = [];
-    if (typeof name === "string") names.push(name);
-    else if (name instanceof Array) names.push(...name);
+    let names: string[] = [];
+    if (name instanceof Array) names = name as never;
     let store: map = this.store as map;
     //初期値が無かった場合に仮設定
     if (store === undefined) {
@@ -136,16 +223,22 @@ export class ReduxModule<State = { [key: string]: unknown }> {
         store = defaultState as never;
       }
     }
-    if (name === undefined) return (store as never) as T;
     //対象のデータを検索
     const length = names.length;
+    if (length === 0) return (store as never) as T;
     let i;
     for (i = 0; i < length - 1; i++) {
       if (store[names[i]] === undefined) return undefined;
       store = store[names[i]] as map;
     }
-    return store[names[i]] as T;
+    return store[names[i]] as Deeps<T, K>;
   }
+
+  // TypeScript3.6では対応できないため保留
+  // public setState<K extends string[], T = State>(
+  //   params: AddOptionType<Deeps<T, K>>,
+  //   ...name: K
+  // ): void;
 
   /**
    *モジュールのStoreデータの設定
@@ -155,39 +248,49 @@ export class ReduxModule<State = { [key: string]: unknown }> {
    * @param {AddOptionType<T>} params
    * @memberof StoreModule
    */
-  public setState<T = State>(
-    name: string | string[],
-    params: AddOptionType<T>
+  public setState<T = State>(params: AddOptionType<T>): void;
+  public setState<K extends string[], T = unknown>(
+    params: unknown,
+    ...name: K
   ): void;
-  public setState<T = { [key: string]: unknown }>(
-    params: AddOptionType<T>
-  ): void;
-  public setState<T = { [key: string]: unknown }>(
-    a: unknown,
-    b?: unknown
+  public setState<K extends string[], T = State>(
+    params: AddOptionType<Deeps<T, K>>,
+    ...name: K
   ): void {
-    if (b === undefined) {
-      this._setState<T>([], a as never);
-    } else {
-      this._setState<T>(a as never, b as never);
-    }
-  }
-  private _setState<T = { [key: string]: unknown }>(
-    name: string | string[],
-    params: AddOptionType<T>
-  ) {
     const storeName = this.moduleName;
     const names = [storeName];
     if (typeof name === "string") names.push(name);
     else if (name instanceof Array) names.push(...name);
-    setStoreState(this.dispatch, names, params);
+    setStoreState(this.dispatch, names, params as never);
   }
 }
-
 //クラス識別用マップ
-const modules = new Map<Function,number>();
+const modules = new Map<Function, number>();
 /**
- *Redux操作用モジュールの生成
+ *ストアに登録するモジュール名を生成する
+ *
+ * @export
+ * @template T
+ * @param {{
+ *     new (dispatch: Dispatch, store: unknown, prefix: string): T;
+ *   }} module
+ * @param {string} [prefix]
+ * @returns {string}
+ */
+export function getStoreName<T extends ReduxModule>(
+  module: moduleType<T>,
+  prefix?: string
+): string {
+  let count = modules.get(module);
+  if (count === undefined) {
+    count = modules.size;
+    modules.set(module, count);
+  }
+  return "@Module-" + (prefix || "") + "-" + count;
+}
+
+/**
+ *Redux操作用モジュールの取得(Hooks用)
  *
  * @export
  * @template T
@@ -195,23 +298,153 @@ const modules = new Map<Function,number>();
  * @param {string} [prefix] Storeデータ分離用装飾文字列
  * @returns {T}
  */
-export function useModule<T extends ReduxModule>(
+export function useModule<T extends ReduxModule, C extends typeof ReduxModule>(
   module: {
-    new (dispatch: Dispatch<any>, store: unknown, prefix: string): T;
-  },
+    new (
+      dispatch: Dispatch,
+      store: unknown,
+      prefix: string,
+      modules: undefined | { [key: string]: ReduxModule }
+    ): T;
+  } & C,
   prefix?: string
 ): T {
-  let count = modules.get(module);
-  if(count === undefined){
-    count = modules.size;
-    modules.set(module,count);
+  const includes = module.includes;
+  let modules: { [key: string]: ReduxModule } = {};
+  if (includes) {
+    includes.forEach(inc => {
+      if (inc instanceof Function) modules[getStoreName(inc)] = useModule(inc);
+      else
+        modules[getStoreName(inc.module, inc.prefix)] = useModule(
+          inc.module,
+          inc.prefix
+        );
+    });
   }
-  const moduleName = "@Module-"+(prefix || "")+"-"+count;
+
+  const moduleName = getStoreName(module, prefix);
+  const store = useSelector(store => (store as map)[moduleName]) as
+    | map
+    | undefined;
   const dispatch = useDispatch();
-  const store = useSelector(
-    store => (store as map)[moduleName]
-  ) as map | undefined;
   return useMemo(() => {
-    return new module(dispatch, store, moduleName);
-  }, [store]);
+    return new module(dispatch, store, moduleName, modules);
+  }, [store, ...Object.values(modules)]);
+}
+/**
+ * モジュールを取得(Classコンポーネント用)
+ *
+ * @export
+ * @template T
+ * @param {unknown} props
+ * @param {moduleType<T>} module
+ * @param {string} [prefix]
+ * @returns
+ */
+export function mapModule<T extends ReduxModule>(
+  props: unknown,
+  module: moduleType<T>,
+  prefix?: string,
+  modules?: {
+    [key: string]: ReduxModule;
+  }
+) {
+  const moduleName = getStoreName(module, prefix);
+  return (props as { modules: map }).modules[moduleName] as T;
+}
+
+/**
+ *Classコンポーネントから機能を利用するときに使用する
+ *
+ * @export
+ * @template T
+ * @template C
+ * @param {C} component
+ * @param {(moduleType<T>
+ *     | { module: moduleType<T>; prefix?: string }
+ *     | (moduleType<T> | { module: moduleType<T>; prefix?: string })[])} module
+ * @returns
+ */
+export function mapConnect<T extends ReduxModule, C extends typeof Component>(
+  component: C,
+  module:
+    | moduleType<T>
+    | { module: moduleType<T>; prefix?: string }
+    | (moduleType<T> | { module: moduleType<T>; prefix?: string })[]
+) {
+  const modules = module instanceof Array ? module : [module];
+
+  function mapStateToProps(state: map) {
+    const newState: map = {};
+
+    function addStoreState(
+      src:
+        | typeof ReduxModule
+        | {
+            module: typeof ReduxModule;
+            prefix?: string | undefined;
+          }
+    ) {
+      let module: typeof ReduxModule;
+      let prefix: string | undefined;
+      if (src instanceof Function) {
+        module = src;
+      } else {
+        module = src.module;
+        prefix = src.prefix;
+      }
+      const moduleName = getStoreName(module, prefix);
+      newState[moduleName] = state[moduleName];
+      if (module.includes) {
+        module.includes.forEach(src => addStoreState(src));
+      }
+    }
+
+    modules.forEach(src => {
+      addStoreState(src);
+    });
+    return newState;
+  }
+
+  function mapMargeToProps(
+    store: map,
+    { dispatch }: { dispatch: Dispatch },
+    props: unknown
+  ) {
+    function addStreModule(
+      state: map,
+      src:
+        | typeof ReduxModule
+        | {
+            module: typeof ReduxModule;
+            prefix?: string | undefined;
+          }
+    ) {
+      let module, prefix;
+      if (src instanceof Function) {
+        module = src;
+      } else {
+        module = src.module;
+        prefix = src.prefix;
+      }
+      const modules = {};
+      if (module.includes) {
+        module.includes.forEach(src => {
+          addStreModule(modules, src);
+        });
+      }
+      const moduleName = getStoreName(module, prefix);
+      const inst = new module(dispatch, store[moduleName], moduleName, modules);
+      (state as map)[moduleName] = inst;
+      return inst;
+    }
+    const state = {};
+    modules.forEach(src => addStreModule(state, src));
+    return { ...store, ...{ modules: state }, ...props };
+  }
+  return connect(
+    mapStateToProps,
+    (dispatch: Dispatch) => ({ dispatch }),
+    mapMargeToProps
+  )(component);
 }
